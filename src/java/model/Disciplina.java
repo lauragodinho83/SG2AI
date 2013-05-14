91bbc115351805a64fe9de4851d22ae4156bf414
jeferson <jeferson@jefersonpc>
2013-05-14 10:17:44 -0300

    Adicionado as entidades professor e disciplina com relacionamento n - n; Apens a model, Assim que possivel faço a view e a controll

---
+++ src/java/model/Disciplina.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author jeferson
+ */
+@Entity
+public class Disciplina implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String descricao;
+    @ManyToMany(mappedBy = "disciplina")
+    private List<Professor> professor;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Professor> getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(List<Professor> professor) {
+        this.professor = professor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Disciplina)) {
+            return false;
+        }
+        Disciplina other = (Disciplina) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Disciplina[ id=" + id + " ]";
+    }
+}
