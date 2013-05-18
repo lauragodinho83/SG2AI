43f071d3367e8dfe2f7c56ccc411140360c501c6
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2013-05-18 00:50:29 -0300

    Daniel Fernandes Silva

--- src/java/controller/ProfessorEJB.java
+++ src/java/controller/ProfessorEJB.java
@@ -9,7 +9,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import model.Disciplina;
 import model.Professor;
 
 /**
